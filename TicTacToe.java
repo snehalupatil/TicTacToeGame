5a82694f017732e5e21eaa379907487211491f86
snehalupatil <patilsnehal4575@gmail.com>
2021-05-09 11:38:06 +0530

    Added: Allow the player to choose X or O

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,42 @@
+import java.util.Scanner;
+
 public class TicTacToe {
-	
-	public char[] creatBoard() 
-	{   
-		char board[] = new char[10];
-		for (int i = 1; i < board.length; i++) 
-		{
+
+	public String playerChoice;
+	public String computerChoice;
+
+	char board[] = new char[10];
+
+	// Creating Empty Board method
+	public char[] creatBoard() {
+		for (int i = 1; i < board.length; i++) {
 			board[i] = ' ';
 		}
 		return board;
 	}
+	
+	//choose letter user and computer choice
+	public String chooseLetter() { 
+		Scanner letter = new Scanner(System.in); // taking user input
+		System.out.println("Enter the letter X-O:");
+		String inputLetter = letter.nextLine();
+		if (inputLetter.equals("X")) {
+			playerChoice = "X";    
+			computerChoice = "O";
+		} else if (inputLetter.equals("O")) {
+			playerChoice = "O";     
+			computerChoice = "X";   
+		} else
+			System.out.println("Invalid symbol...."); // invalid Symbol
+		letter.close();
 
-	public static void main(String[] args) 
-	{
-		System.out.println("Welcome to TicTacToe Game!!");//Welcome message
-		TicTacToe obj = new TicTacToe();//object creation
-		obj.creatBoard();
+		return playerChoice;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("welcome to TicTacToe!!");         // Welcome message
+		TicTacToe tictactoeboard = new TicTacToe();   // object creation
+		tictactoeboard.creatBoard();                          // method call
+		tictactoeboard.chooseLetter();                        // method call
 	}
 }
